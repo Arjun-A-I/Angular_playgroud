f89a723750ea6d19f63ede6df9c24e2849c8d5bd
Arjun-A-I <arjunai5768@gmail.com>
2024-06-19 14:50:41 +0530

    added display functionality

--- appointment-app/src/app/appoinment/appoinment.component.ts
+++ appointment-app/src/app/appoinment/appoinment.component.ts
@@ -8,14 +8,29 @@ import { Appointment } from '../models/appointment';
 })
 export class AppoinmentComponent {
 
-  newAppointment: string = ""
+  newAppointmentTitle: string = ""
   newAppointmentDate: Date = new Date ()
 
   appointment: Appointment []= []
 
   addNewAppointment()
   {
-    alert(this.newAppointment+" "+this.newAppointmentDate)
+if(this.newAppointmentTitle.trim().length && this.newAppointmentDate)
+  {
+      let newAppointment: Appointment={
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+
+      this.appointment.push(newAppointment)
+      this.newAppointmentTitle='';
+      this.newAppointmentDate=new Date();
+
+      alert(this.appointment.length);
+
+       
+    }
   }
 
 }
